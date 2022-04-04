3be6c634f1869c81eaf435d38250d656ced06ca5
Sharlene Kumbhar <shakumbhar@gmail.com>
2022-04-04 22:22:52 +0530

    Sharlene

--- app/src/main/java/com/example/artcasper/Profile.java
+++ app/src/main/java/com/example/artcasper/Profile.java
@@ -1,14 +1,27 @@
 package com.example.artcasper;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 public class Profile extends AppCompatActivity {
-
+    AppCompatButton logoutbutton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logoutbutton=findViewById(R.id.btn_logout);
+        logoutbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(getApplicationContext(),SignInActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
